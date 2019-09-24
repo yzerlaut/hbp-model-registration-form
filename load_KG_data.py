51fb9b14c993f1d9f6f758e9ac2c8e3155cb2431
Yann Zerlaut <yann.zerlaut@gmail.com>
2019-09-24 17:02:52 +0200

    adding the offline db

---
+++ load_KG_data.py
@@ -0,0 +1,47 @@
+import json, os
+import numpy as np
+
+
+uniminds_data, minds_data = {}, {}
+
+NAMESPACES = ['datasets', 'methods', 'activities',
+              'agecategories', 'modalities', 'formats',
+              'parcellationatlases', 'parcellationregions',
+              'persons', 'preparations', 'protocols',
+              'sex', 'species']
+KEYS = ['identifier', 'name', 'description', '@id']
+
+for entry in NAMESPACES:
+    
+    uniminds_data[entry], minds_data[entry] = {}, {}
+    for key in KEYS:
+        minds_data[entry][key] = []
+        uniminds_data[entry][key] = []
+
+    filename = 'KGdb/minds-%s.json' % entry
+    if os.path.isfile(filename):
+        with open(filename) as f:
+            dataJ = json.load(f)
+        minds_dataJ = dataJ['results']
+        
+        for key in KEYS:
+            if key in minds_dataJ[0]:
+                for i in range(len(minds_dataJ)):
+                    minds_data[entry][key].append(minds_dataJ[i][key])
+
+    filename = 'KGdb/uniminds-%s.json' % entry
+    if os.path.isfile(filename):
+        with open(filename) as f:
+            dataJ = json.load(f)
+        uniminds_dataJ = dataJ['results']
+            
+        for key in KEYS:
+            if key in uniminds_dataJ[0]:
+                for i in range(len(uniminds_dataJ)):
+                    uniminds_data[entry][key].append(uniminds_dataJ[i][key])
+                    
+
+for entry in NAMESPACES:
+    print("uniminds %s: %i" % (entry, len(uniminds_data[entry]['name'])))
+    print("minds %s: %i" % (entry, len(minds_data[entry]['name'])))
+
